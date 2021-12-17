4e3e842a534750f6b3ecc7589b84aadcc8ab6685
Alex <alexamuek@gmail.com>
2021-12-17 18:06:25 +0800

    tests

--- tests/test_wrapper_addCollateral_Orig721Wrap721_4.py
+++ tests/test_wrapper_addCollateral_Orig721Wrap721_4.py
@@ -23,11 +23,11 @@ def test_addColl(accounts, erc721mock, wrapper, dai, weth, wnft721, niftsy20, mo
 	mockHacker721_1.mint(accounts[1], 0, {"from": accounts[1]})
 	mockHacker721_1.approve(wrapper.address, 0, {"from": accounts[1]})
 	
-	wrapper.addCollateral(wnft721.address, wTokenId, [((3, mockHacker721_1.address), 0, 0)], {'from': accounts[1]})
+	wrapper.addCollateral(wnft721.address, wTokenId, [((3, mockHacker721_1.address), 0, 0)], {'from': accounts[1], "value": "1 ether"})
 
 	assert mockHacker721_1.balanceOf(accounts[1]) == 0 #check balance
 	assert mockHacker721_1.ownerOf(0) == wrapper #check owner
-	assert wrapper.getWrappedToken(wnft721, wTokenId)[1] == [((3, mockHacker721_1.address), 0, 0)]
+	assert wrapper.getWrappedToken(wnft721, wTokenId)[1] == [((1, zero_address), 0 , Wei("1 ether")), ((3, mockHacker721_1.address), 0, 0)]
 
 	logging.info(wrapper.getWrappedToken(wnft721, wTokenId)[1])
 
