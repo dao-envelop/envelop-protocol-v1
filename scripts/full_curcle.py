9db968233c346b75bb25ccbdcb8683e7550f48e7
Alex <alexamuek@gmail.com>
2021-12-26 00:17:43 +0800

    tests

--- scripts/full_curcle.py
+++ scripts/full_curcle.py
@@ -1,72 +1,87 @@
 import logging
 import time
 from brownie import *
+from web3 import Web3
 LOGGER = logging.getLogger(__name__)
 
 
-#0-0x989FA3062bc4329B2E3c5907c48Ea48a38437fB7
-private_key='721fd469cc7604dcc6ab618e4055822216d977cd14deeae23e6e452c82da9ae9'
+#0-0x989FA3062bc4329B2E3c5907c48Ea48a38437fB7  - leasingPool
+private_key='???'
+accounts.add(private_key)
+
+#1-0xbD7E5fB7525ED8583893ce1B1f93E21CC0cf02F6 
+private_key='???'
+accounts.add(private_key)
+
+#2-0xdf19CC2c917CD70D41071beBE9529E4d58868e27
+private_key='???'
 accounts.add(private_key)
 
 
 def main():
-	wrapperRent     = WrapperForRent.at('0x8904Ef7b93f6aAcbaF66E7Fe61616a92bC73Cfdd')
+	wrapper     = WrapperForRent.at('0x8904Ef7b93f6aAcbaF66E7Fe61616a92bC73Cfdd')
 	wnft1155         = EnvelopwNFT1155.at('0x5bF8D8E84Ae2Ee9514bA37B18cfe1b868992ee6f')
-	original_nft_contract = Bunny.at('0xa3a6e26032c01eac3557207dbfae45d305625ecd')
-	in_type = 3
+	original_nft_contract = Token1155Mock.at('0xbeF5Be938a4417CE626380D9d2dbfAc07256778c')
+	in_type = 4
 	out_type = 4
+	in_nft_amount = 5
 	out_nft_amount = 5
 	
-	original_nft_id = 59
+	original_nft_id = 4
+	price = "2 gwei"
 
-	original_nft_contract.approve(wrapperRent.address, original_nft_id, {"from": accounts[0], "gas_price": price})
+	original_nft_contract.mint(accounts[0], original_nft_id, in_nft_amount, {"from": accounts[0], "gas_price": price})
+	
+	#make allowance to use original NFT
+	original_nft_contract.setApprovalForAll(wrapper.address, True,  {"from": accounts[0], "gas_price": price})
 
-
-	'''token_property = (in_type, erc1155mock)
-	token_data = (token_property, ORIGINAL_NFT_IDs[0], coll_amount)
+	
+	token_property = (in_type, original_nft_contract)
+	token_data = (token_property, original_nft_id, in_nft_amount)
 	
 	fee = []
 	lock = []
 	royalty = []
 
 	wNFT = ( token_data,
-	accounts[2], #leasingPool
-	fee,
-	lock,
-	royalty,
-	out_type,
-	out_nft_amount,
-	Web3.toBytes(0x000F)
+		accounts[0], #leasingPool
+		fee,
+		lock,
+		royalty,
+		out_type,
+		out_nft_amount,
+		Web3.toBytes(0x000F)
 	)
-	logging.info('{}'.format(Web3.toBytes(0x000F)))
+	
+	#wrap NFT
+	wrapper.wrap(wNFT, [], accounts[1], {"from": accounts[0], "gas_price": price})
+	wTokenId = wrapper.lastWNFTId(out_type)[1]
 
-	wrapperRent.wrap(wNFT, [], accounts[3], {"from": accounts[1]})
-	wTokenId = wrapperRent.lastWNFTId(out_type)[1]
-	wnft_pretty_print(wrapperRent, wnft1155, wTokenId)
-	assert erc1155mock.balanceOf(wrapperRent.address, ORIGINAL_NFT_IDs[0]) == coll_amount
-	assert wnft1155.balanceOf(accounts[3], wTokenId) == out_nft_amount
+	assert wnft1155.balanceOf(accounts[1], wTokenId) == out_nft_amount
+	assert original_nft_contract.balanceOf(wrapper.address, original_nft_id) == in_nft_amount
 
+	#try to transfer wrapped NFT
+	try:
+		wnft1155.safeTransferFrom(accounts[1], accounts[0], wTokenId, 1, '', {"from": accounts[1], "gas_price": price})
+	except ValueError as ve:
+		print(ve)
 
-	#refuse to transfer
-	with reverts("Trasfer was disabled by author"):
-	 	wnft1155.safeTransferFrom(accounts[3], accounts[9], wTokenId, 1, '', {"from": accounts[3]})
-
-	#refuse to deposit collateral
-	with reverts("Forbidden add collateral"):
-		wrapperRent.addCollateral(wnft1155.address, wTokenId, [], {"from": accounts[1], "value": "1 ether"})
-
-	#refuse to wrap wNFT
-	wnft1155.setApprovalForAll(wrapperRent, True, {"from": accounts[3]})
-
+	#try to deposit collateral
+	try:
+		wrapper.addCollateral(wnft1155.address, wTokenId, [], {"from": accounts[1], "value": 1, "gas_price": price})
+	except ValueError as ve:
+		print(ve)
+	
+	#####
 	token_property = (in_type, wnft1155)
-	token_data = (token_property, wTokenId, coll_amount)
+	token_data = (token_property, wTokenId, in_nft_amount)
 	
 	fee = []
 	lock = []
 	royalty = []
 
 	wNFT = ( token_data,
-	accounts[2], #leasingPool
+	accounts[0], #leasingPool
 	fee,
 	lock,
 	royalty,
@@ -74,23 +89,37 @@ def main():
 	out_nft_amount,
 	'0'
 	)
+
+	wnft1155.setApprovalForAll(wrapper.address, True,  {"from": accounts[1], "gas_price": price})
+
+	#try to wrap wrapped NFT
+	try:
+		wrapper.wrap(wNFT, [], accounts[1], {"from": accounts[1], "gas_price": price})
+	except ValueError as ve:
+		print(ve)
+
+	###
 	
-	
-	#refuse to wrap again
-	with reverts("Wrap check fail"):
-		wrapperRent.wrap(wNFT, [], accounts[4], {"from": accounts[3]})
+	assert original_nft_contract.balanceOf(wrapper.address, original_nft_id) == in_nft_amount
+	assert wnft1155.balanceOf(accounts[1], wTokenId) == out_nft_amount
 
-	#refuse unwrap by owner
-	with reverts("Only unWrapDestinition can unwrap forbidden wnft"):
-		wrapperRent.unWrap(out_type, wnft1155.address, wTokenId, {"from": accounts[3]})
+	#try to unwrap by borrower
+	try:
+		wrapper.unWrap(out_type, wnft1155.address, wTokenId, {"from": accounts[1], "gas_price": price})
+	except ValueError as ve:
+		print(ve)
 
-	#unwrap by UnwrapDestinition
-	wrapperRent.unWrap(out_type, wnft1155.address, wTokenId, {"from": accounts[2]})
+	#try to unwrap by other user
+	try:
+		wrapper.unWrap(out_type, wnft1155.address, wTokenId, {"from": accounts[2], "gas_price": price})
+	except ValueError as ve:
+		print(ve)
 
+	#unwrap by leasingPool
+	wrapper.unWrap(out_type, wnft1155.address, wTokenId, {"from": accounts[0], "gas_price": price})
 
-	print('*******************************************************************************************')
-	print('****************************************USERDATA in fundHiSTAKING**************************')
-	print('*******************************************************************************************')'''
+	assert original_nft_contract.balanceOf(accounts[0], original_nft_id) == in_nft_amount
+	assert wnft1155.balanceOf(accounts[1], wTokenId) == 0
 
 
 
