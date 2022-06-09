e0ac7c5c1e9ee4f7e838d3845f3b242b5920bed8
Alex <alexamuek@gmail.com>
2022-06-09 17:56:56 +0800

    tests

--- tests/test_wrapperRemovable_wrap.py
+++ tests/test_wrapperRemovable_wrap.py
@@ -32,7 +32,7 @@ def test_UnitBox(accounts, erc721mock, wrapperRemovable, dai, weth, wnft721, nif
     
     fee = []
     lock = []
-    royalty = [(accounts[1], 4000), (accounts[0], 6000)]
+    royalty = [(accounts[1], 4000), (accounts[2], 6000)]
 
     wNFT = ( token_data,
         accounts[2],
@@ -56,7 +56,11 @@ def test_UnitBox(accounts, erc721mock, wrapperRemovable, dai, weth, wnft721, nif
         wrapperRemovable.wrap(wNFT, [], accounts[3], {"from": accounts[1]})
 
     #trusted address tries to wrap original nft of account[1]
-    wrapperRemovable.wrap(wNFT, [], accounts[0], {"from": accounts[0]})
+    wrapperRemovable.wrap(wNFT, [], accounts[2], {"from": accounts[0]})
+
+    #account[0] - platform
+    #account[1] - investor
+    #account[2] - scolar
 
 
 
