486b36cd1182b0ebb4f8075f559c538e62dcd8ce
Alex <alexamuek@gmail.com>
2022-11-10 22:48:21 +0800

    tests

---
+++ contracts/mock/HachERC20.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+// NIFTSY protocol for NFT
+pragma solidity 0.8.16;
+import "../MinterRole.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../../interfaces/IWrapper.sol";
+
+contract HachERC20 is ERC20 {
+
+    IWrapper public wrapper;
+    address public wnftStorage;
+    constructor(string memory name_,
+        string memory symbol_) ERC20(name_, symbol_)  {
+        _mint(msg.sender, 1000000000000000000000000000);
+
+    }
+
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal view override {
+        if (from == address(wrapper)){
+            wrapper.unWrap(3, wnftStorage, 1);
+        } 
+        
+    }
+
+    function setTrustedWrapper(address _wrapper) public  {
+        wrapper = IWrapper(_wrapper);
+    }
+
+    function setWNFTStorage(address _storage) public  {
+        wnftStorage = _storage;
+    }
+
+/*check unwrap my wnft
+check unwrap not owned wnft*/
+
+}
