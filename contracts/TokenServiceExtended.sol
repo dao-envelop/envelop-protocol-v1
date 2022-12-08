4ed0d5bed41b7b6f330ae96dd4846ea64cae1848
niftsy <niftsy@iber.group>
2022-12-08 20:46:10 +0700

    Token service extension for use in nftKiosk

---
+++ contracts/TokenServiceExtended.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+// ENVELOP(NIFTSY) protocol V1 for NFT. Wrapper - main protocol contract
+pragma solidity 0.8.16;
+
+// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// import "../interfaces/IERC20Extended.sol";
+// import "./LibEnvelopTypes.sol";
+// import "../interfaces/IERC721Mintable.sol";
+// import "../interfaces/IERC1155Mintable.sol";
+import "./TokenService.sol";
+
+abstract contract TokenServiceExtended is TokenService {
+	
+
+    function _balanceOf(
+        ETypes.AssetItem memory _assetItem,
+        address _holder
+    ) internal view virtual returns (uint256 _balance){
+        //TODO   think about try catch in transfers
+        uint256 balanceBefore;
+        if (_assetItem.asset.assetType == ETypes.AssetType.NATIVE) {
+            _balance = _holder.balance;
+        
+        } else if (_assetItem.asset.assetType == ETypes.AssetType.ERC20) {
+            _balance = IERC20Extended(_assetItem.asset.contractAddress).balanceOf(_holder);
+        } else if (_assetItem.asset.assetType == ETypes.AssetType.ERC721) {
+            _balance = IERC721Mintable(_assetItem.asset.contractAddress).balanceOf(_holder); 
+        } else if (_assetItem.asset.assetType == ETypes.AssetType.ERC1155) {
+            _balance = IERC1155Mintable(_assetItem.asset.contractAddress).balanceOf(_holder, _assetItem.tokenId);
+        } else {
+            revert UnSupportedAsset(_assetItem);
+        }
+    }
+}
