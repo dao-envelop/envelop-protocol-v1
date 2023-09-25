e96530d70835750a33c9afd75d893afb1b15d984
niftsy <niftsy@iber.group>
2023-09-25 08:32:19 +0700

    sync

--- tests/test_wrapper_users721_m_01.py
+++ tests/test_wrapper_users721_m_01.py
@@ -79,7 +79,7 @@ def test_rules_721(accounts, wrapperUsers, wnft721SBT, erc721mock):
     with reverts('Trasfer was disabled by author'):
         wnft721SBT.transferFrom(accounts[3], accounts[0], 0, {'from': accounts[3]})
     with reverts('UnWrapp forbidden by author'):    
-        wrapperUsers.unWrap(wnft721SBT, 0)
+        wrapperUsers.unWrap(wnft721SBT, 0, {'from': accounts[3]})
 
 def test_add_collateral(accounts, wrapperUsers, wnft721SBT, erc721mock, niftsy20):
     coll = [((2, niftsy20.address), 0, 1e18)]
@@ -88,5 +88,42 @@ def test_add_collateral(accounts, wrapperUsers, wnft721SBT, erc721mock, niftsy20
     wrapperUsers.addCollateral(wnft721SBT,0, coll, {"from": accounts[0]})
     with reverts('Only wNFT contract owner able to add collateral'):
         wrapperUsers.addCollateral(wnft721SBT,0, coll, {"from": accounts[1]})
+
+def test_check_wrap_721_unwrap(
+    accounts, 
+    usersSBTRegistry, 
+    wrapperUsers, wnft721SBT, wnft1155SBT, erc721mock, niftsy20):
+     #without possible rules
+    #makeNFTForTest721(accounts, erc721mock, ORIGINAL_NFT_IDs)
+    erc721mock.transferFrom(accounts[0], accounts[1], ORIGINAL_NFT_IDs[1], {"from": accounts[0]})
+    erc721mock.approve(wrapperUsers.address, ORIGINAL_NFT_IDs[1], {"from": accounts[1]})
+
+    token_data = ((in_type, erc721mock), ORIGINAL_NFT_IDs[1], 0)
+    
+    fee = []
+    lock = []
+    royalty = []
+
+    indata = ( 
+        token_data,   # inAsset
+        accounts[2],  # unWrapDestinition
+        fee,
+        lock,
+        royalty,
+        out_type,
+        0,            # outBalance
+        Web3.toBytes(0x0004)  #rules - NO Unwrap, No Transfer
+    )
+    tx = wrapperUsers.wrapIn(indata, [], accounts[3], wnft721SBT, {"from": accounts[1]})
+    coll = [((2, niftsy20.address), 0, 1e18)]
+    niftsy20.transfer(accounts[1], 1e18, {"from": accounts[0]})
+    niftsy20.approve(wrapperUsers.address, 1e18, {"from": accounts[0]})
+    wrapperUsers.addCollateral(wnft721SBT,1, coll, {"from": accounts[0]})
+    logging.info(tx.return_value)
+    with reverts('Only owner can unwrap it'):
+        tx = wrapperUsers.unWrap(wnft721SBT, 1, {'from': accounts[0]})
+    tx = wrapperUsers.unWrap(wnft721SBT, 1, {'from': accounts[3]})
+    logging.info(tx.events)
+    assert wnft721SBT.balanceOf(accounts[3]) == 1
 
 
