fa681e43fc54e1ecde83a6f1628d3f5d37d8e4d1
niftsy <niftsy@iber.group>
2023-11-26 15:50:25 +0700

    FIX typo in revert message

--- tests/test_wrapper_users721_m_01.py
+++ tests/test_wrapper_users721_m_01.py
@@ -74,9 +74,10 @@ def test_check_wrap_721(accounts, usersSBTRegistry, wrapperUsers, wnft721SBT, wn
         0,            # outBalance
         Web3.toBytes(0x0005)  #rules - NO Unwrap, No Transfer
     )
-    with reverts('Wrap check fail'):
+    with reverts(''):
+        # This revert mean that used Registry has no 
+        # isWrapEnabled method implementation
         wrapperUsers.wrapIn(indata, [], accounts[3], wnft721SBT, {"from": accounts[0]})
-
     
     tx = wrapperUsers.wrapIn(indata, [], accounts[3], wnft721SBT, {"from": accounts[1]})
     logging.info(tx.return_value)
@@ -84,9 +85,9 @@ def test_check_wrap_721(accounts, usersSBTRegistry, wrapperUsers, wnft721SBT, wn
     assert wnft721SBT.totalSupply() == 1
 
 def test_rules_721(accounts, wrapperUsers, wnft721SBT, erc721mock):
-    with reverts('Trasfer was disabled by author'):
+    with reverts('Transfer was disabled by author'):
         wnft721SBT.transferFrom(accounts[3], accounts[0], 0, {'from': accounts[3]})
-    with reverts('UnWrapp forbidden by author'):    
+    with reverts('UnWrap was disabled by author'):    
         wrapperUsers.unWrap(wnft721SBT, 0, {'from': accounts[3]})
 
 def test_add_collateral(accounts, wrapperUsers, wnft721SBT, erc721mock, niftsy20):
