212021fe652f059afc40f654d1b267b525e7c5d2
Alex <alexamuek@gmail.com>
2024-05-26 16:42:06 +0800

    fix test

--- tests/test_wrapper_wrap_Orig1155To1155_8.py
+++ tests/test_wrapper_wrap_Orig1155To1155_8.py
@@ -46,23 +46,23 @@ def test_wrap(accounts, erc1155mock, wrapper, wnft1155, niftsy20):
 	royalty,
 	out_type,
 	out_nft_amount,
-	Web3.to_bytes(0x0008)
+	Web3.toBytes(0x0008)
 	)
 
 	niftsy20.transfer(accounts[1], call_amount, {"from": accounts[0]})
 	niftsy20.approve(wrapper.address, call_amount, {"from": accounts[1]})
 
-	with reverts("Forbidden add collateral"):
-		wrapper.wrap(wNFT, [((2, niftsy20.address), 0, call_amount)], accounts[3], {"from": accounts[1], "value": "1 ether"})
+	wrapper.wrap(wNFT, [((2, niftsy20.address), 0, call_amount)], accounts[3], {"from": accounts[1], "value": "1 ether"})
 
-	'''wTokenId = wrapper.lastWNFTId(out_type)[1]
+	wTokenId = wrapper.lastWNFTId(out_type)[1]
 
 	logging.info("{}".format(wrapper.getWrappedToken(wnft1155.address, wTokenId)))
 
 	niftsy20.transfer(accounts[1], call_amount, {"from": accounts[0]})
-	niftsy20.approve(wrapper.address, call_amount, {"from": accounts[1]})'''
+	niftsy20.approve(wrapper.address, call_amount, {"from": accounts[1]})
 
-	#wrapper.addCollateral(wnft1155.address, wTokenId, [((2, niftsy20.address), 0, call_amount)], {"from": accounts[1]} )
+	with reverts('Forbidden add collateral'):
+		wrapper.addCollateral(wnft1155.address, wTokenId, [((2, niftsy20.address), 0, call_amount)], {"from": accounts[1]} )
 	
 
 	
